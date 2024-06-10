3b053c393cee6ab1377eaae3bf95b1cf180814e3
Enrico <bregolienrico@gmail.com>
2024-06-10 23:42:46 +0200

    [20] aggiunto apiClient e refactor del codice

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import { CACHE_KEY_TODOS } from "../constants";
+import APIClient from "../services/apiClient";
 
+const apiClient = new APIClient<Todo>("/todos");
 export interface Todo {
   id: number;
   title: string;
@@ -10,14 +11,9 @@ export interface Todo {
 }
 
 const useTodos = () => {
-  const fetchTodos = () =>
-    axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data);
-
   return useQuery<Todo[], Error>({
     queryKey: CACHE_KEY_TODOS, // id per la query, usato internamente per il caching
-    queryFn: fetchTodos, // funzione usata per prendere i dati da backend, ritorna una Promise
+    queryFn: apiClient.getAll, // funzione usata per prendere i dati da backend, ritorna una Promise
     staleTime: 10 * 1000, // 10s
   });
 };
---
+++ src/react-query/services/apiClient.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const axiosIstance = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com/",
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = () => {
+    return axiosIstance.get<T[]>(this.endpoint).then((res) => res.data);
+  };
+
+  post = (data: T) => {
+    return axiosIstance.post<T>(this.endpoint, data).then((res) => res.data);
+  };
+}
+
+export default APIClient;
