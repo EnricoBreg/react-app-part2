13da91f607ad43faf567d03f45dc32b9048d1265
Enrico <bregolienrico@gmail.com>
2024-06-16 14:55:37 +0200

    [2.3]: complex actions with reducers

---
+++ src/state-management/reducers/tasksReducer.ts
@@ -0,0 +1,27 @@
+interface Task {
+  id: number;
+  title: string;
+}
+
+interface AddTask {
+  type: "ADD";
+  task: Task;
+}
+
+interface DeleteTask {
+  type: "DELETE";
+  taskId: number;
+}
+
+type TaskAction = AddTask | DeleteTask;
+
+const tasksReducer = (currTasks: Task[], action: TaskAction): Task[] => {
+  switch (action.type) {
+    case "ADD":
+      return [action.task, ...currTasks];
+    case "DELETE":
+      return currTasks.filter((t) => t.id !== action.taskId);
+  }
+};
+
+export default tasksReducer;
